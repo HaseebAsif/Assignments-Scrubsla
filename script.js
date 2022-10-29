1695ba6ac1eda14acf1a9660cc7e43deae714bf7
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-10-29 21:50:20 +0500

    Assignment 6 Completed

--- script.js
+++ script.js
@@ -314,3 +314,57 @@ function addListItem(text) {
   // Add the newly created list item to list
   unorderedList.appendChild(newListItem);
 }
+
+function add() {
+  const value1 = parseInt(document.getElementById("input1").value);
+  const value2 = parseInt(document.getElementById("input2").value);
+
+  const addition = value1 + value2;
+  document.getElementById("sum").innerHTML = addition;
+}
+
+let intCount = 0;
+function increment() {
+  intCount++;
+  document.getElementById(
+    "increment"
+  ).innerHTML = `Button Clicked ${intCount} times`;
+}
+var input = document.getElementById("todo");
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
+
+function item() {
+  let myValue;
+  myValue = document.getElementById("todo").value;
+  console.log(myValue);
+  const newTextNode = document.createTextNode(myValue);
+
+  // Create a new li element
+  const newListItem = document.createElement("li");
+
+  var delButt = document.createElement("button");
+  delButt.appendChild(document.createTextNode("Delete"));
+
+  // Add the text node to the li element
+  newListItem.appendChild(newTextNode);
+  newListItem.appendChild(delButt);
+
+  // Add the newly created list item to list
+  itemList.appendChild(newListItem);
+
+  // Register action on newly added elements
+  delButt.addEventListener("click", deleteListElement);
+  document.getElementById("todo").value = "";
+}
+
+function deleteListElement() {
+  this.parentElement.remove(); // remove parent, since button is child
+}
