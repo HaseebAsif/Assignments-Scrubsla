46fa87af2834ae7e5328efe2472837b3191034d4
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-11-08 15:18:12 +0500

    Assignments separate folders created

---
+++ Assignment 6/script.js
@@ -0,0 +1,24 @@
+const division = (a, b) => {
+  var c = a / b;
+  return c;
+};
+console.log(division(6, 2));
+
+const multiplication = (a, b) => {
+  var c = a * b;
+  return c;
+};
+
+console.log(multiplication(6, 2));
+
+const subtract = (a, b) => {
+  var c = a - b;
+  return c;
+};
+console.log(subtract(6, 2));
+
+const add = (a, b) => {
+  var c = a + b;
+  return c;
+};
+console.log(add(6, 2));
